d8f86f8970f5c95af1109b6eb9c8d31d677628c0
Travis Chapman <travisechapman@gmail.com>
2013-07-12 13:09:19 -0400

    Update weight-watcher.py
    Added a description of the WeightWatcher class

--- weight-watcher.py
+++ weight-watcher.py
@@ -6,9 +6,16 @@ import pylab
 import argparse
 
 class WeightWatcher(object):
+
 	def __init__(self, username, password):
+		'''
+		Takes in 2 strings: username and password for Google Docs
+		
+		'''
+		#use gdata API to access Google Docs
 		self.client = gdata.spreadsheet.service.SpreadsheetsService()
 		self.client.email = username
 		self.client.password = password
+		#self.client.source is the document ID for the shared spreadsheet "Animal Weights after 7-11-13"
 		self.client.source = '0Artit3NRL1oDdFVWRFMyeWpRb005WWsxRjBvQXkteGc'
 		
