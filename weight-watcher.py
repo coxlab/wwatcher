6b911b2aae5082ae97ed579d14c6c068ce92e79b
Travis <travisechapman@gmail.com>
2013-07-13 17:05:26 -0400

    Added .gitignore for pyc files, changed weight-watcher.py

--- weight-watcher.py
+++ weight-watcher.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
 
-import gspread
 import sys
 import pylab
 import argparse
+from spreadsheet import Spreadsheet
 
 class WeightWatcher(object):
 
 	def __init__(self):
 		'''
-		
+		self.data is an instance of the Spreadsheet class
 		
 		'''
-		
+		#TODO make spreadsheet url work here with command line options, as well as alternate sheet names
+		#self.data is an instance of the Spreadsheet class in spreadsheet.py
+		self.data = Spreadsheet(username, password)
+
+
 		
