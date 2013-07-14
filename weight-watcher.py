fa9fa92875ec7f37e79ad09b34b0305b32080c92
Travis <travisechapman@gmail.com>
2013-07-14 04:11:02 -0400

    This will be the basic structure of the script, might put helper functions in another file later

--- weight-watcher.py
+++ weight-watcher.py
@@ -7,14 +7,21 @@ from spreadsheet import Spreadsheet
 
 class WeightWatcher(object):
 
-	def __init__(self):
+	def __init__(self, username, password, spreadsheet_name='Daily Weights after 7-11-13', \
+			spreadsheet_url=None):
 		'''
-		self.data is an instance of the Spreadsheet class
+		
 		
 		'''
-		#TODO make spreadsheet url work here with command line options, as well as alternate sheet names
-		#self.data is an instance of the Spreadsheet class in spreadsheet.py
-		self.data = Spreadsheet(username, password)
-
-
 		
+		#self.data is a list of lists with all the spreadsheet data
+		#e.g. nested list ['date/time', 'username#coxlab.org', 'animal ID', 'weight', 'after water? yes or no'] <--one row
+		self.data = Spreadsheet(username, password, spreadsheet_name, spreadsheet_url).worksheet_open.get_all_values()
+		self.animals_to_analyze = []
+		self.data_list_length = len(self.data)
+	
+	def IsHeavyEnough(self):
+		#go through each aninmal specified by user and make sure it weighs at least 90% its latest max weight
+		
+		def find_max_weight(animal):
+			
